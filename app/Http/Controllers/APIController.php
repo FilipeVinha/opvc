c1d1fdc7cb775ca9be964350b3cd8a937211171e
Henrique <28139@ufp.edu.pt>
2017-05-24 21:49:25 +0100

    Function to insert a new even (unfinished)

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Category;
+use App\Event;
 use App\Local;
 use App\Occurrence;
 use App\User;
@@ -32,12 +33,43 @@ class APIController extends Controller
     {
         $cat_name = $request->input('category');
         $cat = Category::where('category', $cat_name)->first();
-        $subCats = Occurrence::where('category_id', $cat->getAttribute('id')->get());
+        $subCats = Occurrence::where('category_id', $cat->getAttribute('id'))->get();
         return $subCats;
     }
 
     public function getLocals(Request $request)
     {
         return Local::all();
+    }
+
+    public function newEvent(Request $request){
+        $userId = $request->input('user_id');
+        $addr   = $request->input('address');
+        $lat    = $request->input('lat');
+        $lon    = $request->input('lon');
+        $local  = $request->input('local');
+        $cat    = $request->input('category');
+        $subCat = $request->input('subcat');
+        $obs    = $request->input('observations');
+        //falta as fotos
+
+        //ir buscar subCat_id
+        $occr = Occurrence::where('occurrence',$subCat);
+        $lcl = Local::where('local',$local);
+        //ir buscar localType_id
+        //ir buscar timestamp
+
+        $event = new Event();
+        $event->setAttribute("user_id", $userId);
+        $event->setAttribute("address", $addr);
+        $event->setAttribute("lat", $lat);
+        $event->setAttribute("lng", $lon);
+        $event->setAttribute("occurrence_id", $occr->getAttribute('occurrence_id'));
+        $event->setAttribute("local_id", $subCat);
+        $event->setAttribute("obs", $obs);
+
+        $event->save();
+        return "1";
+
     }
 }
